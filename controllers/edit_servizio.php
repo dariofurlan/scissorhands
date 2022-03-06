4ec2d1b786bab61170032932fee3e2d16d267ba8
matt8227 <matteo.bellato20@gmail.com>
2022-03-06 16:44:19 +0100

    Aggiunti messaggi errore

--- controllers/edit_servizio.php
+++ controllers/edit_servizio.php
@@ -43,26 +43,32 @@ function update() {
     } else {$err = "_id"; return $err; }
     if (isset($_POST["name"]) && preg_match('/^[a-zA-Z ]+$/', $_POST["name"])) {
         $book["name"] = $_POST["name"];
-    } else {$err = "name"; return $err; }
+    } else {$err = "Nome del servizio non valido"; return $err; }
     if (isset($_POST["duration"]) && preg_match('/^[0-9]+$/', $_POST["duration"])) {
         $book["duration"] = $_POST["duration"]*60;
-    } else {$err = "duration"; return $err; }
+    } else {$err = "Formato durata non valido"; return $err; }
     if (isset($_POST["price"]) && preg_match('/^[0-9]{1,5}(,[0-9]{1,2})?$/', $_POST["price"])) {
         $book["price"] = $_POST["price"];
-    } else {$err = "price"; return $err; }
+    } else {$err = "Formato del prezzo non valido (servono la virgola)."; return $err; }
     if (isset($_POST["description"]) && preg_match('/^[a-zA-Z \.\,]+$/', $_POST["description"])) {
         $book["description"] = $_POST["description"];
-    } else {$err = "description"; return $err; }
+    } else {$err = "Caratteri non validi nella descrizione"; return $err; }
 
     StaffServiceService::update($book["_id"], $book["name"], $book["price"], $book["duration"], $book["description"]); 
 }
 
 if (isset($_POST) && !empty($_POST)) {
-    update();
+    $res = update();
+	if(!empty($res))
+	{
+		
+		$main = str_replace('%MESSAGGIO%', "<em><span role=\"alert\" id=\"cpw-error\"> $res </span></em>", $main);
+	}
     $service = PublicServiceService::get($_GET["id"]);
 }
 
 
+$main = str_replace('%MESSAGGIO%', '', $main);
 $main = str_replace('%ID%', $service["_id"], $main);
 $main = str_replace('%TYPE%', $service["type"], $main);
 $main = str_replace('%NOME%', $service["name"], $main);
